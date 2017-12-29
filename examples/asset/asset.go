3eda3fc2500c067db887776bb87517b157eb51f5
gzf09 <gaozhenfeng@126.com>
2017-12-29 18:56:44 +0800

    Add range query for asset. Update related test scripts.

--- examples/asset/asset.go
+++ examples/asset/asset.go
@@ -14,6 +14,7 @@ import (
 	"encoding/json"
 	"strings"
 	"math/big"
+	"bytes"
 
 	"github.com/inklabsfoundation/inkchain/core/chaincode/shim"
 	pb "github.com/inklabsfoundation/inkchain/protos/peer"
@@ -28,8 +29,7 @@ const (
 	EditAsset			string = "editAsset"
 	DeleteAsset			string = "deleteAsset"
 	BuyAsset			string = "buyAsset"
-	QueryAssetsByOwner	string = "queryAssetsByOwner"
-	GetHistoryForAsset	string = "getHistoryForAsset"
+	ReadAssetByRange	string = "readAssetByRange"
 )
 
 // Prefixes for user and asset separately
@@ -68,14 +68,14 @@ func main() {
 }
 
 // Init initializes chaincode
-// ===========================
+// ==================================================================================
 func (t *assetChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
 	fmt.Println("assetChaincode Init.")
 	return shim.Success([]byte("Init success."))
 }
 
 // Invoke func
-// ===========================
+// ==================================================================================
 func (t *assetChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 	fmt.Println("assetChaincode Invoke.")
 	function, args := stub.GetFunctionAndParameters()
@@ -140,16 +140,22 @@ func (t *assetChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 		// args[1]: buyer's name
 		return t.buyAsset(stub, args)
 
-		// TODO:
-		//case QueryAssetsByOwner:
-		//case GetHistoryForAsset:
+	case ReadAssetByRange:
+		if len(args) != 2 {
+			return shim.Error("Incorrect number of arguments. Expecting 2.")
+		}
+		// args[0]: startKey
+		// args[1]: endKey
+		return t.readAssetByRange(stub, args)
+
 	}
 
 	return shim.Error("Invalid invoke function name.")
 }
 
+// =============================
 // addUser: Register a new user
-// ===========================
+// =============================
 func (t *assetChaincode) addUser(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	var new_name string
 	var new_age int
@@ -192,8 +198,9 @@ func (t *assetChaincode) addUser(stub shim.ChaincodeStubInterface, args []string
 	return shim.Success([]byte("User register success."))
 }
 
+// ==========================================
 // queryUser: query the information of a user
-// ===========================
+// ==========================================
 func (t *assetChaincode) queryUser(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	user_name := args[0]
 	user_key := UserPrefix + user_name
@@ -210,10 +217,22 @@ func (t *assetChaincode) queryUser(stub shim.ChaincodeStubInterface, args []stri
 	return shim.Success(userAsBytes)
 }
 
+// =============================================
 // addAsset: add a new asset
-// ===========================
+//
+// the name of an asset must start with letters
+// =============================================
 func (t *assetChaincode) addAsset(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	asset_name := args[0]
+
+	// check whether the name starts with letters
+	if len(asset_name) == 0 {
+		return shim.Error("Asset's Name needed.")
+	}
+	if !((asset_name[0] >='a' && asset_name[0]<='z') || (asset_name[0] >='A' && asset_name[0]<='Z')) {
+		return shim.Error("The name of an asset must start with letters.")
+	}
+
 	asset_key := AssetPrefix + asset_name
 
 	asset_type := args[1]
@@ -252,8 +271,9 @@ func (t *assetChaincode) addAsset(stub shim.ChaincodeStubInterface, args []strin
 	return shim.Success([]byte("asset register success."))
 }
 
-// readAsset: query the informatioin of a asset
-// ===========================
+// ===========================================
+// readAsset: query the information of a asset
+// ===========================================
 func (t *assetChaincode) readAsset(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	asset_name := args[0]
 	asset_key := AssetPrefix + asset_name
@@ -270,8 +290,11 @@ func (t *assetChaincode) readAsset(stub shim.ChaincodeStubInterface, args []stri
 	return shim.Success(assetAsBytes)
 }
 
+// ======================================================
 // editAsset: edit the asset
-// ===========================
+//
+// only owner can edit the information of his/her asset
+// ======================================================
 func (t *assetChaincode) editAsset(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	asset_name := args[0]
 	asset_key := AssetPrefix + asset_name
@@ -317,7 +340,7 @@ func (t *assetChaincode) editAsset(stub shim.ChaincodeStubInterface, args []stri
 	return shim.Error("Invalid asset filed name (\"Type\", \"Content\", \"PriceType\" or \"Price\" wanted).")
 
 LABEL_STORE:
-	// store new asset
+// store new asset
 	assetJSONasBytes, err := json.Marshal(new_asset)
 	if err != nil {
 		return shim.Error(err.Error())
@@ -331,8 +354,11 @@ LABEL_STORE:
 	return shim.Success(assetJSONasBytes)
 }
 
+// ======================================
 // delAsset: del a specific asset by name
-// ===========================
+//
+// only owner can delete his/her asset
+// ======================================
 func (t *assetChaincode) delAsset(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	asset_name := args[0]
 	asset_key := AssetPrefix + asset_name
@@ -395,8 +421,9 @@ func (t *assetChaincode) delAsset(stub shim.ChaincodeStubInterface, args []strin
 	return shim.Success([]byte("asset delete success."))
 }
 
+// ==========================================================
 // buyAsset: buy a specific asset, make transfer to the owner
-// =========================================================
+// ==========================================================
 func (t *assetChaincode) buyAsset(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	asset_name := args[0]
 	asset_key := AssetPrefix + asset_name
@@ -492,4 +519,67 @@ func (t *assetChaincode) buyAsset(stub shim.ChaincodeStubInterface, args []strin
 	}
 
 	return shim.Success([]byte("Success buying the asset."))
+}
+
+// ========================================================================
+// readAssetByRange: query assets' names by range (startKey, endKey)
+//
+// startKey and endKey are case-sensitive
+// use "" for both startKey and endKey if you want to query all the assets
+// ========================================================================
+func (t *assetChaincode) readAssetByRange(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+
+	startKey := ""
+	endKey := ""
+	if args[0] != ""{
+		startKey = AssetPrefix + args[0] + "*"
+	}
+	if args[1] != ""{
+		endKey = AssetPrefix + args[1] + "*"
+	}
+
+	resultsIterator, err := stub.GetStateByRange(startKey, endKey)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	defer resultsIterator.Close()
+
+	// buffer is a JSON array containing QueryResults
+	var buffer bytes.Buffer
+	buffer.WriteString("[")
+
+	bArrayMemberAlreadyWritten := false
+	bArrayIndex := 1
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return shim.Error(err.Error())
+		}
+		// check asset key
+		cur_key := queryResponse.Key
+
+		if len(cur_key) >= len(AssetPrefix) && cur_key[0:len(AssetPrefix)] == AssetPrefix {
+			// Add a comma before array members, suppress it for the first array member
+			if bArrayMemberAlreadyWritten == true {
+				buffer.WriteString(",")
+			}
+			// index of the result
+			buffer.WriteString("{\"Number\":")
+			buffer.WriteString("\"")
+			bArrayIndexStr := strconv.Itoa(bArrayIndex)
+			buffer.WriteString(string(bArrayIndexStr))
+			bArrayIndex += 1
+			buffer.WriteString("\"")
+			// information about current asset
+			buffer.WriteString(", \"Record\":")
+			buffer.WriteString(string(queryResponse.Value))
+			buffer.WriteString("}")
+			bArrayMemberAlreadyWritten = true
+		}
+
+	}
+	buffer.WriteString("]")
+
+	return shim.Success(buffer.Bytes())
+
 }

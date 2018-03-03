3881896d360640689875d6c40b381eb9a68c6f1b
zhangtailin <zhangtailin@zhangtailindeMacBook-Pro.local>
2018-03-03 13:06:04 +0800

    modify shameful bug

--- examples/inkwork/inkwork.go
+++ examples/inkwork/inkwork.go
@@ -10,13 +10,11 @@ import (
 	"strconv"
 	"time"
 	"bytes"
-	"math/rand"
 )
 
 type WorkChainCode struct {
 	sysAddress		string
 	TokenType		string		// as "INK"
-	MaxWorkTime		int 		// max auto generate work time, sec
 }
 
 func main() {
@@ -28,19 +26,11 @@ func main() {
 
 func (w *WorkChainCode)Init(stub shim.ChaincodeStubInterface) pb.Response {
 	_, args := stub.GetFunctionAndParameters()
-	if len(args) != 3 {
-		return shim.Error("init, Incorrect number of arguments. Expecting 3")
+	if len(args) != 2 {
+		return shim.Error("init, Incorrect number of arguments. Expecting 2")
 	}
 	w.sysAddress = args[0]
 	w.TokenType = args[1]
-	nTime, err := strconv.Atoi(args[2])
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-	if nTime <= 0 {
-		return shim.Error("init, auto generate work time invalid")
-	}
-	w.MaxWorkTime = nTime
 
 	return shim.Success(nil)
 }
@@ -49,11 +39,11 @@ func (w *WorkChainCode)Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 	function, args := stub.GetFunctionAndParameters()
 
 	switch function {
-	case GetWork:
-		if len(args) != 0 {
-			return shim.Error("getWork, Incorrect number of arguments. Expecting 0")
+	case RegisterWork:
+		if len(args) != 3 {
+			return shim.Error("getWork, Incorrect number of arguments. Expecting 3")
 		}
-		return w.getWork(stub)
+		return w.registerWork(stub, args)
 	case Purchase:
 		if len(args) != 1 {
 			return shim.Error("purchase, Incorrect number of arguments. Expecting 1")
@@ -69,88 +59,105 @@ func (w *WorkChainCode)Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 			return shim.Error("query, Incorrect number of arguments. Expecting 1")
 		}
 		return w.query(stub, args)
+	case QueryInkwork:
+		if len(args) != 1 {
+			return shim.Error("queryInkwork, Incorrect number of arguments. Expecting 1")
+		}
+		return w.queryInkwork(stub, args)
 	}
 
 	return shim.Error("function is invalid: " + function)
 }
 
-func (w *WorkChainCode) workState(stub shim.ChaincodeStubInterface, workId string) (work WorkDef, err error) {
+func (w *WorkChainCode) workState(stub shim.ChaincodeStubInterface, workId string) (work *WorkDef, err error) {
 	var valAsbytes []byte
-	work = WorkDef{}
-	valAsbytes, err = stub.GetState(workId)
-	if err != nil {
+	if valAsbytes, err = stub.GetState(workId); err != nil {
 		// Failed to get state
-		return
+		return nil, err
 	} else if valAsbytes == nil {
 		// work does not exist
 		fmt.Printf("%s work does not exist", workId)
-		return
+		return nil, nil
 	}
-
-	err = json.Unmarshal([]byte(valAsbytes), &work)
+	work = &WorkDef{}
+	err = json.Unmarshal([]byte(valAsbytes), work)
 	return
 }
 
-func (w *WorkChainCode) getWork(stub shim.ChaincodeStubInterface) pb.Response {
-	sender, err := stub.GetSender()
-	if err != nil {
+//@param workId
+//@param level
+//@param price
+func (w *WorkChainCode) registerWork(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	workId := args[0]
+	var level, price int
+	var err error
+	if level, err = strconv.Atoi(args[1]); err != nil{
+		return shim.Error(err.Error())
+	}
+	if price, err = strconv.Atoi(args[2]); err != nil {
+		return shim.Error(err.Error())
+	}
+
+	var sender string
+	if sender, err = stub.GetSender(); err != nil {
 		return shim.Error("Fail to get sender's address.")
 	}
 	sender = strings.ToLower(sender)
 
-	bGet, err := w.getFreeState(stub, sender)
-	if err != nil {
+	if bGet, err := w.getFreeState(stub, sender); err != nil {
 		return shim.Error("Fail to getFreeState")
-	}
-	if bGet == true {
+	} else if bGet == true {
 		return shim.Error("already get")
 	}
 
-	workId, level := w.autoWork(stub)
-	if workId == "" {
-		return shim.Error("getWork, auto generate work failed.")
+	if data, err := w.workState(stub, workId); err != nil {
+		return shim.Error(err.Error())
+	} else if data != nil {
+		return shim.Error("this work already exist")
 	}
 
+	txTimestamp, err := stub.GetTxTimestamp()
+	if err != nil {
+		return shim.Error("GetTxTimestamp failed")
+	}
 	work := &WorkDef {
 		WorkId:		workId,
 		Level: 		level,
-		Birth:		time.Now().Unix(),
+		Birth:		txTimestamp.String(),
 		Owner:		sender,
 		Sale:		0,
-		Price:		0,
+		Price:		price,
 		SaleTime:	0,
 	}
 
-	workJSONasByte, err := json.Marshal(work)
-	if err != nil {
+	if workJSONasByte, err := json.Marshal(work); err == nil {
+		if err = stub.PutState(workId, workJSONasByte); err != nil {
+			return shim.Error(err.Error())
+		}
+	} else {
 		return shim.Error(err.Error())
 	}
-	err = stub.PutState(workId, workJSONasByte)
-	if err != nil {
-		return shim.Error(err.Error())
-	}
+
 	err = w.historyByComposite(stub, FreeHistory, []string{sender, workId})
 	if err != nil {
 		return shim.Error(err.Error())
 	}
 
-	result := fmt.Sprintf("get work:%v Succeed", workId)
+	result := fmt.Sprintf("register work:%v Succeed", workId)
 	return shim.Success([]byte(result))
 }
 
-func (w *WorkChainCode) historyByComposite(stub shim.ChaincodeStubInterface, recordType string, args []string) error {
+func (w *WorkChainCode) historyByComposite(stub shim.ChaincodeStubInterface, recordType string, args []string) (err error) {
 	var indexKey string
-	var err error
-	indexKey, err = stub.CreateCompositeKey(recordType, args)
-	if err != nil {
-		return err
+	if indexKey, err = stub.CreateCompositeKey(recordType, args); err != nil {
+		return
 	}
+
 	value := []byte{0x00}
-	err = stub.PutState(indexKey, value)
-	if err != nil {
-		return err
+	if err = stub.PutState(indexKey, value); err != nil {
+		return
 	}
-	return nil
+	return
 }
 
 func (w *WorkChainCode) getFreeState(stub shim.ChaincodeStubInterface, user string) (bool, error) {
@@ -180,11 +187,15 @@ func (w *WorkChainCode) getFreeState(stub shim.ChaincodeStubInterface, user stri
 //@param workId
 func (w *WorkChainCode) purchase(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	workId := args[0]
-	work, err := w.workState(stub, workId)
-	if err != nil {
+	var data *WorkDef
+	var err error
+	if data, err = w.workState(stub, workId); err != nil {
 		return shim.Error(err.Error())
+	} else if data == nil {
+		return shim.Error("this work not exist")
 	}
-	if 0 == work.Sale {
+
+	if 0 == data.Sale {
 		jsonResp := "{\"Error\":\"buy failed, work does not sale: " + workId + "\"}"
 		return shim.Error(jsonResp)
 	}
@@ -194,35 +205,32 @@ func (w *WorkChainCode) purchase(stub shim.ChaincodeStubInterface, args []string
 		return shim.Error("Fail to get sender's address.")
 	}
 	sender = strings.ToLower(sender)
-	if sender == work.Owner {
+	if sender == data.Owner {
 		jsonResp := "{\"Error\":\"buy failed, already is work owner\"}"
 		return shim.Error(jsonResp)
 	}
 	balance := w.getBalance(stub, sender)
-	if balance < int64(work.Price) {
+	if balance < int64(data.Price) {
 		return shim.Error("bid failed, balance not enough")
 	}
 
 	// pay cost
 	amount := big.NewInt(0)
-	_, good := amount.SetString(strconv.Itoa(work.Price), 10)
+	_, good := amount.SetString(strconv.Itoa(data.Price), 10)
 	if !good  {
 		return shim.Error("Expecting integer value for amount")
 	}
-	err = stub.Transfer(work.Owner, w.TokenType, amount)
+	err = stub.Transfer(data.Owner, w.TokenType, amount)
 	if err != nil {
 		return shim.Error("transfer error: " + err.Error())
 	}
 
-	work.Owner = sender
-	work.Sale = 0
-	work.SaleTime = 0
-	workAsbytes, err := json.Marshal(work)
-	if err != nil {
+	data.Owner = sender
+	data.Sale = 0
+	data.SaleTime = 0
+	if workAsbytes, err := json.Marshal(data); err != nil {
 		return shim.Error(err.Error())
-	}
-	err = stub.PutState(workId, workAsbytes)
-	if err != nil {
+	} else if err = stub.PutState(workId, workAsbytes); err != nil {
 		return shim.Error(err.Error())
 	}
 
@@ -249,15 +257,18 @@ func (w *WorkChainCode) getBalance(stub shim.ChaincodeStubInterface, userAddr st
 //@param sellState
 func (w *WorkChainCode) sell(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	workId := args[0]
+	var work *WorkDef
+	var err error
 	sender, err := stub.GetSender()
 	if err != nil {
 		return shim.Error("Fail to get sender's address.")
 	}
 	sender = strings.ToLower(sender)
 
-	work, err := w.workState(stub, workId)
-	if err != nil {
+	if work, err = w.workState(stub, workId); err != nil {
 		return shim.Error(err.Error())
+	} else if work == nil {
+		return shim.Error("this work not exist")
 	}
 
 	if sender != work.Owner {
@@ -286,13 +297,10 @@ func (w *WorkChainCode) sell(stub shim.ChaincodeStubInterface, args []string) pb
 	work.Sale = sale
 	work.Price = price
 	work.SaleTime = saleTime
-	workJSONasByte, err := json.Marshal(work)
-	if err != nil {
+	if workJSONasByte, err := json.Marshal(work); err != nil {
 		return shim.Error(err.Error())
-	}
-	err = stub.PutState(workId, workJSONasByte)
-	if err != nil {
-		return shim.Error(err.Error())
+	} else if err = stub.PutState(workId, workJSONasByte); err != nil {
+			return shim.Error(err.Error())
 	}
 
 	result := fmt.Sprintf("sell work:%v, sale:%v, price:%v", workId, sale, price)
@@ -370,150 +378,17 @@ func (w *WorkChainCode)query(stub shim.ChaincodeStubInterface, args []string) pb
 	return shim.Success([]byte(buffer.String()))
 }
 
-func (w *WorkChainCode)timeDuration(srcTime int64) int {
-	return int(time.Now().Unix() - srcTime)
+//@param workId
+func (w *WorkChainCode) queryInkwork(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	workId := args[0]
+	valAsbytes, err := stub.GetState(workId)
+	if err != nil {
+		jsonResp := "{\"Error\":\"Failed to get state for " + workId + "," + err.Error() + "\"}"
+		return shim.Error(jsonResp)
+	} else if valAsbytes == nil {
+		jsonResp := "{\"Error\":\"cat does not exist: " + workId + "\"}"
+		return shim.Error(jsonResp)
+	}
+
+	return shim.Success(valAsbytes)
 }
-
-func (w *WorkChainCode) autoWork(stub shim.ChaincodeStubInterface)(gene string, level int) {
-	curTime := time.Now().Unix()
-	for {
-		if w.timeDuration(curTime) > w.MaxWorkTime {
-			break
-		}
-		gene, level  = w.newGenome()
-		// gene min length is 28
-		if len(gene) < 28 {
-			continue
-		}
-		catAsbytes, err := stub.GetState(gene)
-		if err == nil && catAsbytes == nil {
-			break
-		}
-	}
-
-	return
-}
-
-func (w *WorkChainCode)newGenome() (string, int) {
-	var buffer bytes.Buffer
-	level := 0
-	shape, index := w.rateRand(2,[]int{30,20,20,30})
-	if shape == "" {
-		return "", 0
-	}
-	if index == 1 || index == 2 {
-		level += 2
-	}
-	buffer.WriteString(shape)
-
-	line, index := w.rateRand(1,[]int{60,40})
-	if line == "" {
-		return "", 0
-	}
-	if index == 1 {
-		level += 2
-	}
-	buffer.WriteString(line)
-	shade, nColor := w.rateRand(1,[]int{60,30,10})
-	if shade == "" {
-		return "", 0
-	}
-	if nColor == 1 {
-		level += 3
-	}else if nColor == 2 {
-		level += 5
-	}
-	buffer.WriteString(shape)
-	//angle
-	buffer.WriteString(w.limitRand(36000,5,1,35999))
-	//draw
-	buffer.WriteString(w.limitRand(10000,4,1,9999))
-	//with
-	buffer.WriteString(w.limitRand(900,3,1,899))
-	for i:=0; i<=nColor; i++ {
-		color , n := w.colorRand()
-		level += n
-		buffer.WriteString(color)
-	}
-
-	return buffer.String(), level
-}
-
-//rate total 100
-//@param byte num
-//@param per type rate
-//@return bytes
-//@return level
-func (w *WorkChainCode)rateRand(length int, rateSlice []int) (string, int) {
-	var buffer bytes.Buffer
-	num := len(rateSlice)
-	if num <=0 {
-		return "", 0
-	}
-	val := rand.Intn(100)
-	nCount := 0
-	for i:=0; i<num; i++ {
-		nCount += rateSlice[i]
-		if val < nCount {
-			valStr := strconv.Itoa(i)
-			if len(valStr) < length {
-				for j:=0; j<length-len(valStr); j++ {
-					buffer.WriteString("0")
-				}
-			}
-			buffer.WriteString(valStr)
-			return buffer.String(), i
-		}
-	}
-
-	return "", 0
-}
-
-func (w *WorkChainCode)limitRand(num, length, min, max int) string {
-	if num <= 0 || length <= 0{
-		return ""
-	}
-	if min >= max {
-		return ""
-	}
-
-	var buffer bytes.Buffer
-	val := 0
-	for {
-		val = rand.Intn(num)
-		if val < min || val > max {
-			continue
-		}
-		break
-	}
-	valStr := strconv.Itoa(val)
-	if len(valStr) < length {
-		for i:=0; i<length-len(valStr); i++ {
-			buffer.WriteString("0")
-		}
-	}
-	buffer.WriteString(valStr)
-	return buffer.String()
-}
-
-func (w *WorkChainCode)colorRand() (string, int) {
-	var buffer bytes.Buffer
-	var level int
-	r := w.limitRand(255,3,1,254)
-	g := w.limitRand(255,3,1,254)
-	b := w.limitRand(255,3,1,254)
-	a := w.limitRand(101,3,1,100)
-	if r == g && r == b {
-		level += 2
-	}else if (r != g && r != b && g != b) {
-		level = 0
-	}else {
-		level += 1
-	}
-	buffer.WriteString(r)
-	buffer.WriteString(g)
-	buffer.WriteString(b)
-	buffer.WriteString(a)
-	return buffer.String(), level
-}
-

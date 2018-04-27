b6fdfc7afaf4a2599fe2b8cb8a916d9d020ed275
EvansChang <85141291@qq.com>
2018-04-27 15:22:34 +0800

    update ledger_util add cross_transfer info update ledger_interface add CrossTransfer function

--- core/ledger/kvledger/txmgmt/ledgerutil/ledger_proto_util.go
+++ core/ledger/kvledger/txmgmt/ledgerutil/ledger_proto_util.go
@@ -5,11 +5,13 @@ import (
 	"github.com/inklabsfoundation/inkchain/core/ledger/kvledger/txmgmt/rwsetutil"
 	"github.com/inklabsfoundation/inkchain/core/ledger/kvledger/txmgmt/transutil"
 	"github.com/inklabsfoundation/inkchain/protos/ledger"
+	"github.com/inklabsfoundation/inkchain/core/ledger/kvledger/txmgmt/ctransutil"
 )
 
 type LedgerSet struct {
-	TranSet *transutil.TranSet
-	TxRwSet *rwsetutil.TxRwSet
+	TranSet      *transutil.TranSet
+	TxRwSet      *rwsetutil.TxRwSet
+	CrossTranSet *ctransutil.CrossTranSet
 }
 
 func (ledgerSet *LedgerSet) ToProtoBytes() ([]byte, error) {
@@ -24,6 +26,7 @@ func (ledgerSet *LedgerSet) ToProtoBytes() ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
+	protoLedgerSet.Crosstranset, err = ledgerSet.CrossTranSet.ToProtoBytes()
 	protoLedgerSetBytes, err := proto.Marshal(protoLedgerSet)
 	return protoLedgerSetBytes, nil
 }
@@ -45,6 +48,19 @@ func (ledgerSet *LedgerSet) FromProtoBytes(protoBytes []byte) error {
 		}
 	} else {
 		ledgerSet.TranSet = nil
+	}
+
+	if protoLedgerSet.Crosstranset != nil {
+		ledgerSet.CrossTranSet = &ctransutil.CrossTranSet{}
+		err = ledgerSet.CrossTranSet.FromProtoBytes(protoLedgerSet.Crosstranset)
+		if err != nil {
+			return err
+		}
+		if ledgerSet.CrossTranSet.TokenType == "" {
+			ledgerSet.CrossTranSet = nil
+		}
+	} else {
+		ledgerSet.CrossTranSet = nil
 	}
 	ledgerSet.TxRwSet = &rwsetutil.TxRwSet{}
 	err = ledgerSet.TxRwSet.FromProtoBytes(protoLedgerSet.Txrwset)

6d65247cccf120a31575f9d01d4b0ec637433955
mahuaibo <ma_huaibo@qq.com>
2018-06-12 15:42:53 +0800

    v1.0.2

---
+++ examples/xc/qtum-v1.0.2/Token.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.19;
+
+contract Token {
+
+    function transfer(address to, uint value) external returns (bool);
+
+    function transferFrom(address from, address to, uint value) external returns (bool);
+
+    function balanceOf(address owner) external view returns (uint);
+
+    function allowance(address owner, address spender) external view returns (uint);
+}
